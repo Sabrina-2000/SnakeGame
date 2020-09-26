c640f35058f75a6e615d63f005d46f955e5307ee
youngkang98 <youngkanglau@gmail.com>
2020-09-26 18:09:42 +0800

    Add Food to the game

---
+++ SnakeGame/SnakeGame/Food.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class Food
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public Food()
+        {
+            Random random = new Random();
+            this.x = random.Next(Console.WindowWidth);
+            this.y = random.Next(Console.WindowHeight);
+        }
+
+        public void Generate_random_food()
+        {
+            Console.SetCursorPosition(this.x, this.y);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("@");
+        }
+    }
+}

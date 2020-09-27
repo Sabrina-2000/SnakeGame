b2452f069cf7feec67ed6b2e9a21e8bd0e91ab9c
Sabrina Tan <sabrinatzt2000@gamil.com>
2020-09-27 10:10:27 +0800

    update

--- SnakeGame/SnakeGame/Obstacle.cs
+++ SnakeGame/SnakeGame/Obstacle.cs
@@ -1,10 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Threading;
 
 namespace SnakeGame
 {
     class Obstacle
     {
+        private List<Position> obstacles;
+        private List<int> _x;
+        private List<int> _y;
+
+        public void GenerateObstacles()
+        {
+            Random random = new Random();
+            obstacles = new List<Position>();
+            _x = new List<int>();
+            _y = new List<int>();
+
+            for (int i = 1; i < 2; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                int x = random.Next(Console.WindowHeight);
+                int y = random.Next(Console.WindowWidth);
+                Position positions = new Position(x, y);
+                obstacles.Add(positions);
+            }
+        }
+
+        public List<Position> GetObstacles
+        {
+            get { return obstacles; }
+        }
     }
 }
--- SnakeGame/SnakeGame/Snake.cs
+++ SnakeGame/SnakeGame/Snake.cs
@@ -6,5 +6,26 @@ namespace SnakeGame
 {
     class Snake
     {
+        public Queue<Position> snake;
+
+        public Snake()
+        {
+            snake = new Queue<Position>();
+        }
+
+        public Queue<Position> GetPos
+        {
+            get { return snake; }
+        }
+
+        public void DrawSnake()
+        {
+            int i = 0;
+            while (i <= 5)
+            {
+                snake.Enqueue(new Position(0, i));
+                i++;
+            }
+        }
     }
 }
